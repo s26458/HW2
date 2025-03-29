5072e781cacf91134e51d29b1f8ecc91939fa764
s26458 <s26458@pjwstk.edu.pl>
2025-03-29 19:20:02 +0100

    Liquid_container

--- Cwiczenie2_rider/Cwiczenie2_rider/Program.cs
+++ Cwiczenie2_rider/Cwiczenie2_rider/Program.cs
@@ -1,4 +1,6 @@
-﻿public class OverfillException : Exception
+﻿using System.Runtime.InteropServices;
+
+public class OverfillException : Exception
 {
     public OverfillException(string message) : base(message) { }
 }
@@ -47,3 +49,34 @@ public abstract class Container
         return $"Serial: {SerialNumber}, Load: {CurrentLoad}/{MaxCapacity}kg, Weight: {Weight}kg";
     }
 }
+
+public class LiquidContainer : Container, IHazardNotifier
+{
+    public bool IsHazardous {get; private set;}
+    
+    public LiquidContainer(double maxCapacity, double weight, double height, double depth, bool isHazardous) 
+        : base(maxCapacity, weight, height, depth, "L")
+    {
+        IsHazardous = isHazardous;
+        ProductType = isHazardous ? "Hazardous Liquid" : "Regular Liquid";
+    }
+
+    public override void Load(double mass)
+    {
+        double effectriveCapacity = IsHazardous ? MaxCapacity * 0.5 / MaxCapacity * 0.9;
+        if (mass + CurrentLoad > effectriveCapacity)
+            throw new OverfillException($"Cannot load {mass}kg it is more than {effectriveCapacity}kg.");
+        base.Load(mass);
+    }
+
+    public void NotifyHazardous(string message)
+    {
+        Console.WriteLine($"HAZARD ALERT [{SerialNumber}]: {message}");
+    }
+
+    public void NotifyHazard(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
+
