1332625fdb94685f869174a57d555511c019d8fd
s26458 <s26458@pjwstk.edu.pl>
2025-03-29 19:11:44 +0100

    Klasa_bazowa_container

--- Cwiczenie2_rider/Cwiczenie2_rider/Program.cs
+++ Cwiczenie2_rider/Cwiczenie2_rider/Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class OverfillException : Exception
+{
+    public OverfillException(string message) : base(message) { }
+}
 
-Console.WriteLine("Hello, World!");
+public interface IHazardNotifier
+{
+    void NotifyHazard(string message);
+}
+
+public abstract class Container
+{
+    public string SerialNumber { get; private set; }
+    public double MaxCapacity { get; private set; } 
+    public double CurrentLoad { get; protected set; } 
+    public double Weight { get; private set; } 
+    public double Height { get; private set; } 
+    public double Depth { get; private set; } 
+    protected string ProductType { get; set; }
+
+    private static int serialCounter = 1;
+
+    public Container(double maxCapacity, double weight, double height, double depth, string typeCode)
+    {
+        SerialNumber = $"KON-{typeCode}-{serialCounter++}";
+        MaxCapacity = maxCapacity;
+        Weight = weight;
+        Height = height;
+        Depth = depth;
+        CurrentLoad = 0;
+    }
+
+    public virtual void Load(double mass)
+    {
+        if (mass + CurrentLoad > MaxCapacity)
+            throw new OverfillException($"Cannot load {mass}kg. Exceeds max capacity of {MaxCapacity}kg");
+        CurrentLoad += mass;
+    }
+
+    public virtual void Unload()
+    {
+        CurrentLoad = 0;
+    }
+
+    public override string ToString()
+    {
+        return $"Serial: {SerialNumber}, Load: {CurrentLoad}/{MaxCapacity}kg, Weight: {Weight}kg";
+    }
+}
